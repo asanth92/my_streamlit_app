bb3f80b74a3d502de2dc4bcab40ae16db774bacd
Arati Santhanakrishnan <asanthanakrishnan@MBPl20C02DK1P6P3Y0.local>
2022-11-09 06:37:13 -0800

    added requirements and pickle file for model

--- nyc_app_complete.py
+++ nyc_app_complete.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import streamlit as st
 import joblib
+import sklearn
 
 st.write('Streamlit is an open-source app framework for Machine Learning and Data Science teams. For the docs, please click [here](https://docs.streamlit.io/en/stable/api.html). \
     This is is just a very small window into its capabilities.')
